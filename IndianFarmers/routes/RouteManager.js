cfdb6415360d087ec0d51060572f79fb9d21f9e5
amitSA <send.amit@yahoo.com>
2016-01-31 08:01:54 -0800

    added all static files, did nott include the data dumpp file thing

---
+++ IndianFarmers/routes/RouteManager.js
@@ -0,0 +1,55 @@
+//arjun's 169.233.193.211
+var app = require("../app");
+var mysql = require("mysql");
+
+var tableName = "list";
+
+
+var connection = mysql.createConnection( {
+   host: "169.233.193.211",
+   user: "test",
+   password: "arjun123",
+   database: "u-farm"
+});
+
+var endConnection = function (conn) {
+   conn.end(function (err) {
+      if (err) {
+         console.log("Connectino Unsucessfully ended");
+         return;
+      }
+         // The connection is terminated gracefully
+         // Ensures all previously enqueued queries are still
+         // before sending a COM_QUIT packet to the MySQL server.
+   });
+}
+
+
+app.get("/query", function (req, res, next) {
+   console.log("\n req ip address: " + req.ip);
+   var connection = mysql.createConnection({
+      host: "169.233.193.211",
+      user: "test",
+      password: "arjun123",
+      database: "u-farm"
+   });
+   connection.connect(function (err) {
+      if (err) {
+         console.log("Error connection to database, error: " + err);
+         return;
+      }
+      console.log("Connection Succesfully Established");
+      connection.query("SELECT * FROM " + tableName, function (err, rows) {
+         if (err) {
+            console.log("Error in queing from table, error: " + err);
+         } else {
+            console.log("Recieved Data from table: \n" + JSON.stringify(rows,null,2));
+         }
+         //end connection
+         endConnection(connection);
+         res.send(rows);
+      });
+   });
+   // res.send("<h1>In Search route</h1>")
+   //areq.params.category
+});
